88163c61465cd9b3df4f06c3b6836c680c01ab06
Avihay23 <avihaybartal@gmail.com>
2023-11-23 10:25:56 +0200

    Notification + Alarm done + Broadcast receiver + Share Prefrence

--- app/src/main/java/com/example/potionemporium/MainActivity4.java
+++ app/src/main/java/com/example/potionemporium/MainActivity4.java
@@ -5,6 +5,7 @@ package com.example.potionemporium;
 
         import android.content.Intent;
         import android.content.IntentFilter;
+        import android.content.SharedPreferences;
         import android.net.ConnectivityManager;
         import android.os.Bundle;
         import android.view.View;
@@ -12,17 +13,44 @@ package com.example.potionemporium;
 
 public class MainActivity4 extends AppCompatActivity {
     private NetworkStateReceiver networkStateReceiver;
-    private boolean checkData = false;
-    private Button btn;
+    private Button btn,btn2;
+    private SharedPreferences ison;
+    private boolean checkData;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         btn = (Button) findViewById(R.id.receiverbtn);
+        btn2 = (Button) findViewById(R.id.leavenow);
+        ison = getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
+        SharedPreferences.Editor editor =ison.edit();
+        checkData = ison.getBoolean("datacheck",false);
+        if (ison.getBoolean("datacheck",false)){
+            btn.setText("Turn off Data Receiver");
+            networkStateReceiver = new NetworkStateReceiver();
+            IntentFilter connectFilter = new IntentFilter();
+            connectFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+            registerReceiver(networkStateReceiver, connectFilter);
+        }
+
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void onOff(View view) {
+        ison = getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
+        SharedPreferences.Editor editor =ison.edit();
         if (!checkData) {
             networkStateReceiver = new NetworkStateReceiver();
 
@@ -31,12 +59,17 @@ public class MainActivity4 extends AppCompatActivity {
 
             registerReceiver(networkStateReceiver, connectFilter);
             checkData = true;
+            editor.putBoolean("datacheck",true);
+            editor.commit();
             btn.setText("Turn off Data Receiver");
         } else {
             unregisterReceiver(networkStateReceiver);
             checkData = false;
+            editor.putBoolean("datacheck",false);
+            editor.commit();
             btn.setText("Turn on Data Receiver");
         }
+
     }
 
     @Override
@@ -45,13 +78,15 @@ public class MainActivity4 extends AppCompatActivity {
         if (checkData) {
             unregisterReceiver(networkStateReceiver);
         }
-
     }
+    @Override
     protected void onResume() {
         super.onResume();
-
-        IntentFilter networkFilter = new IntentFilter();
-        registerReceiver(networkStateReceiver, networkFilter);
+        if(ison.getBoolean("datacheck",false)==true) {
+            IntentFilter connectFilter2 = new IntentFilter();
+            connectFilter2.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+            registerReceiver(networkStateReceiver, connectFilter2);
+        }
 
     }
 }
